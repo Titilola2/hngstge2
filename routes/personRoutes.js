04cff3f986294fe383403291fcbb4a7740b8c0a8
hokageCodes <hashsenju94@gmail.com>
2023-09-12 00:31:04 +0100

    CRUD Done

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -5,20 +5,20 @@ const personController = require('../controllers/personController');
 const personRoutes = express.Router();
 
 // Create a new person
-personRoutes.post('/person', personController.createPerson);
+personRoutes.post('/', personController.createPerson);
 
 // Fetch details of person by ID
-personRoutes.get('/person/:id', personController.getPersonById);
+personRoutes.get('/:id', personController.getPersonById);
 
 // Fetch details of person by name
-personRoutes.get('/person', [
+personRoutes.get('/', [
     query('name').isString().notEmpty(),
 ], personController.getPersonByName);
 
 // Update details of an existing person by ID
-personRoutes.put('/person/:id', personController.updatePerson);
+personRoutes.put('/:id', personController.updatePerson);
 
 // Remove a person by ID
-personRoutes.delete('/person/:id', personController.deletePerson);
+personRoutes.delete('/:id', personController.deletePerson);
 
 module.exports = personRoutes;
